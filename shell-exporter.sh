fa1a92045ca1acb458f85fa5bc435542b6add0b6
Sabry Razick <sabryr@gmail.com>
2019-03-25 11:31:19 +0100

    Chekcing command validity before commiting

--- shell-exporter.sh
+++ shell-exporter.sh
@@ -4,24 +4,36 @@
 #so students can follow, if fall behind
 #for this to work, history must be redirected as follows
 #export HISTFILE=$SHARED_HISTORY_FILE
+#On the working terminal 
+# export  PROMPT_COMMAND="history -a"
+#export  SHARED_HISTORY_FILE=<the histotory file>
+# export HISTFILE=$SHARED_HISTORY_FILE
  
 export PS1="\# "
 last_printed="NA"
-WEB_SERVER_DIR=""
+WEB_SERVER_DIR="pariganaka.uio.no:~/www_docs/"
 i=1;
 echo "<html> <head> <title>History</title> <meta http-equiv=\"refresh\" content=\"3\" />  </head>  <body><ol type=\"1\">" > history.html
 while [ $? -eq "0" ]; 
 do
 	last=$(tail -n 1 $SHARED_HISTORY_FILE)
-	if [ "$last" != "$last_printed" ] 
-	then
-		echo $i". "$last
-		sed -i -- '/\/body/d'  history.html
-		echo "<li>$last</li>" >> history.html
-		echo " </ol> </body> </html>" >> history.html
-		scp history.html $WEB_SERVER_DIR
-		last_printed="$last"
-		i=$((i+1))
+	last_base=$(echo $last | awk '{print $1}')
+	which "$last_base"  > /dev/null 2>&1
+	if [  $? -eq "0" ] 
+        then
+		if [ "$last" != "$last_printed" ] 
+		then
+			echo $i". "$last
+			sed -i -- '/\/body/d'  history.html
+			echo "<li>$last</li>" >> history.html
+			echo " </ol> </body> </html>" >> history.html
+			scp history.html $WEB_SERVER_DIR > /dev/null 2>&1
+			last_printed="$last"
+			i=$((i+1))
+		fi
+	#else
+	#	echo "Wrong command"
 	fi
-sleep 3
+
+sleep 2
 done;
