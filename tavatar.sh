6bf052a8e67d72b51f013fbf18446f28cd28c31a
Sabry Razick <sabryr@gmail.com>
2019-11-07 11:20:47 +0100

    Functioning test version

--- tavatar.sh
+++ tavatar.sh
@@ -10,6 +10,7 @@
 
 DOC_NM="1VQ4lUr1hrrKQwOAYa2z0h-0xltBm1PEuQLY8VksWk7c"
 VERSION="0.1"
+ENV_NM="$HOME/.tavatar_env"
 connect () {
 	echo "function"
 	export PS1="\# "
@@ -22,17 +23,21 @@ connect () {
 		last=$(tail -n 1 $SHARED_HISTORY_FILE)
         	last_base=$(echo $last | awk '{print $1}')
         	which "$last_base"  > /dev/null 2>&1
-        	if [[  $? -eq "0" ]] || [[ "$last" == *http* ]]
+		if [[  $? -eq "0" ]] || [[ "$last" == *http* ]] || [[ "$last" == *STOP* ]]
         	then
-                	if [ "$last" != "$last_printed" ]
+			if [[ "$last_base" == "STOP" ]]
+			then
+				echo "END" >> $SHARED_HISTORY_FILE
+				exit 0
+			elif [ "$last" != "$last_printed" ]
                 	then
                         	last_printed="$last"
 				echo "$last_printed"
-				send-togoogle-client $last_printed
+				send-togoogle-client "$last_printed"
                         elif [[ "$last" == *http* ]]
                         then
 				echo "$last_printed"
-				send-togoogle-client $last_printed
+				send-togoogle-client "$last_printed"
                                 #last_printed= "$last"
 				#last_m=$(echo "$last" | sed "s/#//")
                 		#echo "<li><a href=\"$last_m\"> $last_m</a></li>" >> history.html
@@ -53,12 +58,42 @@ setterm () {
 }
 
 send-togoogle-client () {
-	 python  google-client.py $DOC_NM 1 $1
+	 python  google-client.py $DOC_NM 1 "$1"
 }
+
+reset () {
+	echo "History cleaned"
+        rm $HOME/.online_history
+}
+
+create_env () {
+	ls $ENV_NM  > /dev/null 2>&1
+	if [[ !   $? -eq "0" ]]
+	then
+		echo "Creating Python environemnt "
+		tavatar_dir=$(which tavatar.sh) 
+		echo "tavetar in $tavatar_dir"
+		#virtualenv $ENV_NM
+		source $ENV_NM/bin/activate
+		pip install -r requirements.txt
+	else
+		source $ENV_NM/bin/activate
+	fi
+}
+
+help () {
+	echo "Usage"
+	echo "Open two terminals and on on"
+	echo "./tavatar.sh MAIN"
+	echo "On the other"
+	echo "./tavatar.sh CONNECT <Google doc ID>"
+	
+}
+create_env
 
 if [[ $# -eq 0 ]] 
 then
-	echo "Print help"
+	help
 elif [ "$1" == "-v" ]
 then
 	echo "Version $VERSION"
@@ -69,8 +104,14 @@ then
 	setterm
 elif [ "$1" == "CONNECT" ]
 then
-	echo "Connect"
-	connect
+	if [[ $# -eq 2 ]]
+	then
+		DOC_NM="$2"
+		echo "Connect"
+		connect
+	else
+		help
+	fi
 elif [ "$1" == "CLEAN" ]
 then
 	echo "History cleaned"
