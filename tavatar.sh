cbd55fe817e7c7e2210185fc3302133a640447df
Sabry Razick <sabryr@gmail.com>
2019-11-07 10:11:59 +0100

    Version with debug prints

--- tavatar.sh
+++ tavatar.sh
@@ -16,42 +16,37 @@ connect () {
 	export HISTFILE=$SHARED_HISTORY_FILE
 	last_printed="NA"
 	i=1;
-	#echo "<html> <head> <title>History</title> <meta http-equiv=\"refresh\" content=\"3\" />  </head>  <body><ol type=\"1\">" > history.html
 	while [ $? -eq "0" ]; 
 	do
-        	last=$(tail -n 1 $SHARED_HISTORY_FILE)
+		last=$(tail -n 1 $SHARED_HISTORY_FILE)
         	last_base=$(echo $last | awk '{print $1}')
         	which "$last_base"  > /dev/null 2>&1
         	if [[  $? -eq "0" ]] || [[ "$last" == *http* ]]
         	then
                 	if [ "$last" != "$last_printed" ]
                 	then
-                        	echo "$last"
-				#echo $i". "$last
-                        	#sed -i -- '/\/body/d'  history.html
-                        if [[ "$last" == *http* ]]
+                        	last_printed="$last"
+				echo "$last_printed"
+                        elif [[ "$last" == *http* ]]
                         then
-                                last_m=$(echo "$last" | sed "s/#//")
+				echo "$last_printed"
+                                #last_printed= "$last"
+				#last_m=$(echo "$last" | sed "s/#//")
                 		#echo "<li><a href=\"$last_m\"> $last_m</a></li>" >> history.html
-                        else	
-				echo "else"
-                                #echo "<li>$last</li>" >> history.html
                         fi
-                        echo "end"
-			#echo " </ol> </body> </html>" >> history.html
-                        #scp history.html $WEB_SERVER_DIR > /dev/null 2>&1
-                        last_printed="$last"
                         i=$((i+1))
-                	fi
         	fi
 		sleep 2
+
 	done;
 }
 
 setterm () {
-	export  PROMPT_COMMAND="history -a"
-	export  SHARED_HISTORY_FILE=$HOME/.online_history
-	export HISTFILE=$SHARED_HISTORY_FILE
+	echo "#Tavatar connect" > $HOME/.tavatar
+	echo "export  PROMPT_COMMAND=\"history -a\"" >> $HOME/.tavatar 
+	echo "export  SHARED_HISTORY_FILE=$HOME/.online_history" >> $HOME/.tavatar
+	echo "export  HISTFILE=$HOME/.online_history" >> $HOME/.tavatar
+	echo "source $HOME/.tavatar"
 }
 
 if [[ $# -eq 0 ]] 
@@ -69,6 +64,10 @@ elif [ "$1" == "CONNECT" ]
 then
 	echo "Connect"
 	connect
+elif [ "$1" == "CLEAN" ]
+then
+	echo "History cleaned"
+	rm $HOME/.online_history
 else
 	echo "Print help"
 fi
