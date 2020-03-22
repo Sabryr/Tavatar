641778b8c90368cad7db8be3df2710b8357af2ef
Richard Darst <rkd@zgib.net>
2020-03-22 17:50:28 +0200

    google-client.py: change to use argparse

--- google-client.py
+++ google-client.py
@@ -16,38 +16,6 @@ DOCUMENT_ID = ''
 DOCUMENT_INDEX = -1
 DOCUMENT_TEXT = ''
 
-def get_doc_id():
-  """Read in google document id.
-    """
-  if len(sys.argv) > 1:
-     DOCUMENT_ID = sys.argv[1]
-     return DOCUMENT_ID
-  else:
-    print("print usage")
-    sys.exit()
-
-def get_doc_index():
-  """Read in google document possition .
-    to place the text """
-
-  if len(sys.argv) > 2:
-     DOCUMENT_INDEX = sys.argv[2]
-     return DOCUMENT_INDEX
-  else:
-    print("print usage")
-    sys.exit()
-
-def get_doc_text():
-  """Read in google document text .
-    to place the text """
-
-  if len(sys.argv) > 3:
-     DOCUMENT_TEXT = sys.argv[3]
-     return DOCUMENT_TEXT
-  else:
-    print("print usage")
-    sys.exit()
-
 def auth():
     creds = None
     # If the authentication has been performed already
@@ -105,7 +73,14 @@ def insert(service):
 def _text():
   print("implement test")
 if __name__ == '__main__':
-   DOCUMENT_ID = get_doc_id()
-   DOCUMENT_INDEX = get_doc_index()
-   DOCUMENT_TEXT = get_doc_text()
-   insert(auth())
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('doc_id')
+    parser.add_argument('doc_index')
+    parser.add_argument('doc_text')
+    args = parser.parse_args()
+
+    DOCUMENT_ID = args.doc_id
+    DOCUMENT_INDEX = args.doc_index
+    DOCUMENT_TEXT = args.doc_text
+    insert(auth())
